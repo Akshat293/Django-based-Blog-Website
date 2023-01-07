9d61a12e8ae48b01b9522b916282baa4c6758357
Akshat Saxena <76419575+tom635@users.noreply.github.com>
2023-01-07 20:11:38 +0530

    Migrating to the postgresql database

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(%%ths%pp@2-2_n3uwo(6wdthkyx&ncd!nh*($bxaz-sqe&0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG = True
 
-ALLOWED_HOSTS = ['sana-blogs.azurewebsites.net','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "akshatsaxena",
+        'USER': "postgres" ,
+        'PASSWORD': "postgres",
+        'HOST': "localhost",
+        'PORT': "",
     }
 }
 
